f5bdcf16e0f16f20cf9a0133d187b313f4d84b0a
Progga <proggaroy2000@gmail.com>
2022-05-04 23:54:47 +0600

    conditional rander added

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -3,13 +3,25 @@ import TShirt from '../component/TShirt/TShirt';
 import './Cart.css'
 
 const Cart = ({cart,handleRemoveFromCart}) => {
-    
+    let command;
+    if (cart.length===0) {
+        command= <p>Please add at least one item</p>
+    }
+    else if (cart.length===1) {
+        command= <p>Please add one more...</p>
+    }
+    else{
+        command= <p>Thanks for adding item</p>
+    }
+
    
     return (
+      
         <div>
+             <h1>Item selected :{cart.length} </h1>
            
-           <h1>Item selected :{cart.length} </h1>
-           
+             {command}
+          
            
            {
                cart.map(tShirt => <p>{tShirt.name}
